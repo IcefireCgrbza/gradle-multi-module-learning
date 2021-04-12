7516cc3a26a0e4807e949336721eea656fb1659c
feimao <feimao@pinduoduo.com>
2021-04-12 21:21:16 +0800

    gradle multi module

---
+++ service/src/main/java/service/Greeter.java
@@ -0,0 +1,10 @@
+package service;
+
+import lib.GreetingFormatter;
+
+public class Greeter {
+    public static void main(String[] args) {
+        final String output = GreetingFormatter.greeting(args[0]);
+        System.out.println(output);
+    }
+}
